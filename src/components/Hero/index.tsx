9bc764a68383c29c20e073d95b0e217dca6af6d5
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-08-03 21:20:54 -0300

    add: hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,15 @@
-import styles from './Hero.module.css'
+import { HeroSection, HeroContainer, HeroTitle } from './Hero'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <HeroSection>
+    <HeroContainer>
+      <HeroTitle>
+        As melhores vagas para tecnologia,
+        <br />
+        design e artes visuais.
+      </HeroTitle>
+    </HeroContainer>
+  </HeroSection>
 )
 
 export default Hero
