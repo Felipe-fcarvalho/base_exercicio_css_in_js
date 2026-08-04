ed5d1e6977e40c7ae1428c8c9d9ae492a179e992
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-08-03 21:23:01 -0300

    update

---
+++ src/components/FormVagas/FormVagas.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: var(--cor-secundaria);
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
+`
+
+const BtnPesquisar = styled.button`
+  background-color: var(--cor-principal);
+  border: 1px solid var(--cor-principal);
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: var(--cor-secundaria);
+  margin-left: 8px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`
+
+const Campo = styled.input`
+  padding: 0 16px;
+  outline-color: var(--cor-principal);
+`
+export { Form, BtnPesquisar, Campo }
---
+++ src/components/Hero/Hero.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+export const HeroSection = styled.section`
+  height: 360px;
+  width: 100%;
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  background-position: top;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: var(--cor-principal);
+    opacity: 0.7;
+    z-index: 1;
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
+  }
+`
+
+export const HeroContainer = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  position: relative;
+  color: #eee;
+  z-index: 2;
+`
+
+export const HeroTitle = styled.h1`
+  font-family: Gloock, serif;
+  font-size: 48px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`
